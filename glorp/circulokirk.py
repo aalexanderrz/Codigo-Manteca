958c51dd456fb2975c9f6667dfb211484193c5be
riveraaz <rz22004@ues.edu.sv>
2026-04-11 08:03:11 -0600

    si

--- glorp/circulokirk.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-#t = np.linspace(0,2*np.pi,100)
-#plt.plot(np.cos(t),np.sin(t),color='pink',linewidth=3)
-#plt.show()
-
-n = np.random.rand(5,2)
-print(n[0][0])
